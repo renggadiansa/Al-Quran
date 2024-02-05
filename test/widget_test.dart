ae29f8e4ea0f79f4467f001a78801049cc7d4f6c
renggadiansa <renggadiansa@gmail.com>
2024-02-05 18:07:53 +0700

    widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,108 @@
+import 'package:alquran/app/data/models/detail_surah.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:alquran/app/data/models/ayat.dart';
-import 'package:http/http.dart' as http;
+// [
+//   [........], juz 1 => index 0
+//   [.........], juz 2 => index 1
+//   ...
+//   [........], juz 30 => index 29
+// ]
+
+
+
+// [
+//   {
+//     "juz": 1,
+//     "start": Ayat,
+//     "end": Ayat,
+//     "verses" : [......],
+//   },
+  // .......
+//   {
+//     "juz": 30,
+//     "start": Ayat,
+//     "end": Ayat,
+//     "verses" : [......],
+//   },
+// ]
+
+// List<Map<String, dynamic>>
+
 
 void main() async {
-  var res = await http.get(Uri.parse("https://api.quran.gading.dev/surah/108/1"));
+  int juz = 1;
 
-  Map <String, dynamic> data = jsonDecode(res.body)["data"];
-  // Map <String, dynamic> dataToMode = {
-  //   "number":data["number"],
-  //   "meta":data["meta"],
-  //   "text":data["text"],
-  //   "translation":data["translation"],
-  //   "audio":data["audio"],
-  //   "tafsir":data["tafsir"],
-  // };
+  List<Map<String, dynamic>> penampungAyat = [];
+  List<Map<String, dynamic>> allJuz = [];
 
+  for (var i = 0; i < 114; i++) {
+    var res = await http.get(Uri.parse("https://api.quran.gading.dev/surah/$i"));
+    
+    // Periksa jika respon HTTP berhasil
+    if (res.statusCode == 200) {
+      Map<String, dynamic> rawData = json.decode(res.body)["data"];
 
-  Ayat ayat = Ayat.fromJson(data);
-  // print(ayat.tafsir?.id?.short);
-  print(ayat.audio);
+      // Periksa jika rawData tidak null
+      if (rawData != null) {
+        DetailSurah data = DetailSurah.fromJson(rawData);
 
+        if (data.verses != null) {
+          data.verses!.forEach((ayat) {
+            if (ayat.meta?.juz == juz) {
+              penampungAyat.add({
+                "surah": data.name?.transliteration?.id ?? '',
+                "ayat": ayat,
+              });
+            } else {
+              print("========");
+              print("Berhasil memasukan juz $juz");
+              if (penampungAyat.isNotEmpty) {
+                print("START...");
+                print("ayat: ${(penampungAyat[0]["ayat"] as Verse)?.number?.inSurah}");
+                print((penampungAyat[0]["ayat"] as Verse)?.text?.arab);
+                print("END...");
+                print("ayat: ${(penampungAyat[penampungAyat.length - 1]["ayat"] as Verse)?.number?.inSurah}");
+                print((penampungAyat[penampungAyat.length - 1]["ayat"] as Verse)?.text?.arab);
+              }
+              allJuz.add({
+                "juz": juz,
+                "juzStartInfo": penampungAyat.isNotEmpty ? penampungAyat[0] : null,
+                "juzEndInfo": penampungAyat.isNotEmpty ? penampungAyat[penampungAyat.length - 1] : null,
+                "verses": penampungAyat,
+              });
+              juz++;
+              penampungAyat.clear();
+              penampungAyat.add({
+                "surah": data.name?.transliteration?.id ?? '',
+                "ayat": ayat,
+              });
+            }
+          });
+        }
+      } else {
+        print("Error: Response body is null.");
+      }
+    } else {
+      // Tangani error respon HTTP
+      print("Error fetching data: ${res.statusCode}");
+    }
+  }
 
+  print("========");
+  print("Berhasil memasukan juz $juz");
+  if (penampungAyat.isNotEmpty) {
+    print("START...");
+    print("ayat: ${(penampungAyat[0]["ayat"] as Verse)?.number?.inSurah}");
+    print((penampungAyat[0]["ayat"] as Verse)?.text?.arab);
+    print("END...");
+    print("ayat: ${(penampungAyat[penampungAyat.length - 1]["ayat"] as Verse)?.number?.inSurah}");
+    print((penampungAyat[penampungAyat.length - 1]["ayat"] as Verse)?.text?.arab);
+  }
+  allJuz.add({
+    "juz": juz,
+    "start": penampungAyat.isNotEmpty ? penampungAyat[0] : null,
+    "end": penampungAyat.isNotEmpty ? penampungAyat[penampungAyat.length - 1] : null,
+    "verses": penampungAyat,
+  });
 }
